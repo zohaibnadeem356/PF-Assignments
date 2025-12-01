044d95e6f72cbafb2f876001db7e89758948cc12
Muhammad Zohaib Nadeem <zohaibnadeem3560@gmail.com>
2025-12-01 22:44:53 +0500

    Update Question3.cpp

--- Assignment 3/Question3.cpp
+++ Assignment 3/Question3.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int inches;
+    cout << "Enter height in inches: ";
+    cin >> inches;
+
+    int feet = inches / 12;
+    int r_inches = inches % 12;
+
+    cout << "Height: " << feet << " feet " << r_Inches << " inches" << endl;
+    return 0;
+}
