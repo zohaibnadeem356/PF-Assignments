34c0a1c9175d71c0cb7ba77637540ccb51d3f686
Muhammad Zohaib Nadeem <zohaibnadeem3560@gmail.com>
2025-12-02 07:12:24 +0500

    Update Question3.cpp

--- Assignment 3/Question3.cpp
+++ Assignment 3/Question3.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int main() {
     int inches;
-    cout << "Enter height in inches: ";
-    cin >> inches;
+    cout<<"Enter height in inches: ";
+    cin>>inches;
 
     int feet = inches / 12;
     int r_inches = inches % 12;
 
-    cout << "Height: " << feet << " feet " << r_Inches << " inches" << endl;
+    cout<<"Height: "<<qfeetq<<" feet "<< r_inches<<" inches"<<endl;
     return 0;
 }
